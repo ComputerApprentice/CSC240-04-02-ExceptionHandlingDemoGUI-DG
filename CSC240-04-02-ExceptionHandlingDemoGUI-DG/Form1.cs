328810b0191932653b298c5250318bbff7908c4c
Dixon Geiser <DGEISE2490@my.sullivan.edu>
2024-07-21 11:54:35 -0400

    Add project files.

---
+++ CSC240-04-02-ExceptionHandlingDemoGUI-DG/Form1.cs
@@ -0,0 +1,10 @@
+namespace CSC240_04_02_ExceptionHandlingDemoGUI_DG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
